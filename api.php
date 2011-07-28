745365b1a841a025b3c7aad0e93ad8e66d343b6c
BaylorRae <baylorrae@gmail.com>
2011-07-27 20:26:32 -0500

    Security fix for multiple files.

--- api.php
+++ api.php
@@ -3,8 +3,13 @@
 require 'CSS_Compress.php';
 
 if ( isset($_GET['file_names']) ) {
-   if ( file_exists($_GET['file_names']) ) die('Bad boy');
+   
       $file_names = explode( ',', $_GET['file_names'] );  
+      
+      // Prevent access to local files
+      foreach( $file_names as $file )
+        if ( file_exists($file) ) die('Bad boy');
+        
       $C = new CSS_Compress( $file_names, 'style_min.css' );
       
       if ( isset($_GET['save_as']) ) {
@@ -12,7 +17,10 @@ if ( isset($_GET['file_names']) ) {
       } else $C->save_as();
    
 } else if ( isset($_GET['css']) ) {
+  
+      // Prevent access to local files
       if ( file_exists($_GET['css']) ) die('Bad boy');
+      
       $C = new CSS_Compress( $_GET['css'], 'style_min.css' ); 
       echo $C->get_css();
 }
